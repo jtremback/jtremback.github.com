667713593e50f16b97432e01669f56709c92a627
Jehan Tremback <jehan.tremback@gmail.com>
2012-04-06 15:19:59 -0700

    third commit

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -2,24 +2,32 @@ $(document).ready(function() {
   $(".heatmap").hide();
   $(".bucket").hide();
   $(".overlay").hide();  
+  $(".input-menu").hide();
+  $(".annotate-input").hide();
   $(".detail").hide();
   $("#icon1").click(function() {
     $(".heatmap").slideDown();
   });
   $("#pointer1").click(function() {
     $(".bucket").show();
-    $(".heatmap").css("margin-right","15em");
+    $(".heatmap").css("margin-right","300px");
   });
   $("#card1").click(function() {
     $(".detail").show();
     $(".overlay").show();
-    $(".heatmap").css("margin-right","30em");
-    $(".bucket").css("margin-right","15em");
+    $(".heatmap").css("margin-right","450px");
+    $(".bucket").css("margin-right","150px");
   });
   $(".overlay").click(function() {
     $(".detail").hide();
     $(".overlay").hide();
     $(".heatmap").css("margin-right","15em");
     $(".bucket").css("margin-right","0em");
+  });
+  $(".input").hover(function() {
+    $(".input-menu").slideDown();
+  });
+  $(".annotate").click(function() {
+    $(".annotate-input").slideDown();
   });
 });
