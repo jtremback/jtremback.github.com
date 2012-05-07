60482b8e8b1bec7b3561a245d174afdcfbad79e3
Jehan Tremback <jehan.tremback@gmail.com>
2012-05-07 11:56:35 -0700

    snapshot  master@build-8080b4f

---
+++ build-8080b4f/javascripts/plugins.js
@@ -0,0 +1,6 @@
+//http://djpate.com/2009/10/07/animated-scroll-to-anchorid-function-with-jquery/
+
+function goToByScroll(id){
+  $('html,body').animate({scrollTop: $("#"+id).offset().top},'slow');
+}
+;
